f1506e79dd071f7093366427f69ca191a597a3c9
leonvallejos <dev.leonvallejos@gmail.com>
2023-02-26 20:08:05 -0300

    Add TwitterFollowCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import './App.css'
+import TwitterFollowCard from './components/TwitterFollowCard'
 
 export default function App() {
   return (
-    <article className='tw-followCard'>
-      <header className='tw-followCard-header'>
-        <img className='tw-followCard-avatar' src="https://unavatar.io/leonvallejos" alt="avatar-leonvallejos" />
-        <div className='tw-followCard-info'>
-          <strong>Leon Vallejos</strong>
-          <span tw-followCard-infoUserName>@leonvallejos</span>
-        </div>
-      </header>
-      <aside>
-        <button className='tw-followCard-button'>Seguir</button>
-      </aside>
-    </article>
+    <TwitterFollowCard/>
   )
 
 }
---
+++ src/components/TwitterFollowCard.jsx
@@ -0,0 +1,16 @@
+export default function TwitterFollowCard(){
+  return(
+    <article className='tw-followCard'>
+    <header className='tw-followCard-header'>
+      <img className='tw-followCard-avatar' src="https://unavatar.io/leonvallejos" alt="avatar-leonvallejos" />
+      <div className='tw-followCard-info'>
+        <strong>Leon Vallejos</strong>
+        <span tw-followCard-infoUserName>@leonvallejos</span>
+      </div>
+    </header>
+    <aside>
+      <button className='tw-followCard-button'>Seguir</button>
+    </aside>
+  </article>
+  )
+}
