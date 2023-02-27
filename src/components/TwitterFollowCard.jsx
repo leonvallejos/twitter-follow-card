b2cc553122661b79e0255b934fd1eddb7805739f
leonvallejos <dev.leonvallejos@gmail.com>
2023-02-27 02:39:43 -0300

    Add props for component

--- src/components/TwitterFollowCard.jsx
+++ src/components/TwitterFollowCard.jsx
@@ -1,11 +1,11 @@
-export default function TwitterFollowCard(){
+export default function TwitterFollowCard({userName, name, isFollowing}){
   return(
     <article className='tw-followCard'>
     <header className='tw-followCard-header'>
-      <img className='tw-followCard-avatar' src="https://unavatar.io/leonvallejos" alt="avatar-leonvallejos" />
+      <img className='tw-followCard-avatar' src={`https://unavatar.io/${userName}`} alt="avatar-leonvallejos" />
       <div className='tw-followCard-info'>
-        <strong>Leon Vallejos</strong>
-        <span tw-followCard-infoUserName>@leonvallejos</span>
+        <strong>{name}</strong>
+        <span tw-followCard-infoUserName>@{userName}</span>
       </div>
     </header>
     <aside>
