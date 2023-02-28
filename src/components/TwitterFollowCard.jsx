db508aaa70ee691d16130e7589be5473da46e054
leonvallejos <dev.leonvallejos@gmail.com>
2023-02-28 10:15:56 -0300

    Fix style userName

--- src/components/TwitterFollowCard.jsx
+++ src/components/TwitterFollowCard.jsx
@@ -5,7 +5,7 @@ export default function TwitterFollowCard({userName, name, isFollowing}){
       <img className='tw-followCard-avatar' src={`https://unavatar.io/${userName}`} alt="avatar-leonvallejos" />
       <div className='tw-followCard-info'>
         <strong>{name}</strong>
-        <span tw-followCard-infoUserName>@{userName}</span>
+        <span className="tw-followCard-infoUserName">{`@${userName}`}</span>
       </div>
     </header>
     <aside>
